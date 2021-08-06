6a3066341748199c7232f3433236ea0df8c37193
Genoos <65106962+Genoos@users.noreply.github.com>
2021-08-07 00:19:17 +0530

    Add files via upload

---
+++ lib/services/firestorage.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/cupertino.dart';
+
+import 'firebase_file.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+
+class FirebaseApi with ChangeNotifier{
+  static Future<List<String>> _getDownloadLinks(List<Reference> refs) =>
+      Future.wait(refs.map((ref) => ref.getDownloadURL()).toList());
+
+  static Future<List<FirebaseFile>> listAll(String path) async {
+    final ref = FirebaseStorage.instance.ref(path);
+    final result = await ref.listAll();
+
+    final urls = await _getDownloadLinks(result.items);
+
+    return urls
+        .asMap()
+        .map((index, url) {
+      final ref = result.items[index];
+      final name = ref.name;
+      final file = FirebaseFile(ref: ref, name: name, url: url);
+
+      return MapEntry(index, file);
+    })
+        .values
+        .toList();
+  }
+
+
+  }
